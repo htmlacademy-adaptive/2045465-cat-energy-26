9f451078d263d10e136e18865dad49adf936878c
Инна Дементьева <acorp.murka@gmail.com>
2022-08-30 14:31:28 +0300

    добавляет задачу в gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -134,6 +134,7 @@ export const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/script.js', gulp.series(scripts));
   gulp.watch('source/*.html', gulp.series(html, reload));
+  gulp.watch('source/img/icons/*.svg', gulp.series(sprite));
   // gulp.watch('source/*.html').on('change', browser.reload);
 }
 
